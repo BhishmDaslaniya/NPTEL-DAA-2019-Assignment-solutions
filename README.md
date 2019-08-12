8f69bf38428baa0f82368aceb9859e12a50d9ef8
Bhishm <bhishm1999@gmail.com>
2019-08-12 22:23:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NPTEL-DAA-2019-Assignment-solutions
+ This contains solutions of all programming assignmnets of Design and Analysis course NPTEL 2019.
---
+++ week2_Voting_In_Siruseri.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+
+int main(){
+    ll n[5];
+    for(ll i = 0 ; i < 5 ; i++){
+        cin>>n[i];
+    }
+    
+    return 0;
+}
